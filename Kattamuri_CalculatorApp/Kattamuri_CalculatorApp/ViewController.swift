e280a66a3717ba9a54eaabf5e5391029f89c2eaf
Vyshnavi1410 <69514165+Vyshnavi1410@users.noreply.github.com>
2023-02-14 23:17:28 -0600

    Assignment

--- Kattamuri_CalculatorApp/Kattamuri_CalculatorApp/ViewController.swift
+++ Kattamuri_CalculatorApp/Kattamuri_CalculatorApp/ViewController.swift
@@ -8,12 +8,93 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var resultOutlet: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func buttonAC(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonC(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonPlusOrMinus(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonDivide(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonSeven(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonEight(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonNine(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonX(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonFour(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonFive(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonSix(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonMinus(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonOne(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonTwo(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonThree(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonPlus(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonZero(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonDot(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonModulo(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonEqual(_ sender: UIButton) {
+    }
 }
 
