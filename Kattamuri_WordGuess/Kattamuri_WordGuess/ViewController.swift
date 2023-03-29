db7d2dc9f3aa0a72c8056dadc1acee8a45f08192
Vyshnavi1410 <69514165+Vyshnavi1410@users.noreply.github.com>
2023-03-29 16:17:54 -0500

    Add files via upload

--- Kattamuri_WordGuess/Kattamuri_WordGuess/ViewController.swift
+++ Kattamuri_WordGuess/Kattamuri_WordGuess/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var wordsGuessedLabel: UILabel!
+    
+    
+    @IBOutlet weak var wordsRemainingLabel: UILabel!
+    
+    
+    @IBOutlet weak var totalWordsLabel: UIStackView!
+    
+    
+    @IBOutlet weak var userGuessLabel: UILabel!
+    
+    
+    @IBOutlet weak var guessLetterField: UITextField!
+    
+    
+    @IBOutlet weak var hintLabel: UILabel!
+    
+    
+    
+    @IBOutlet weak var guessCountLabel: UILabel!
+    
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    
+    @IBOutlet weak var displayImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
 
+    @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+    }
+    
+    
 }
 
