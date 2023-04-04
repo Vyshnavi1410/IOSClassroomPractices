54ff73b403ba34f74a5739c3dfae12c051f30074
Vyshnavi1410 <69514165+Vyshnavi1410@users.noreply.github.com>
2023-04-04 09:13:34 -0500

    Add files via upload

---
+++ StudentCourses/StudentCourses/Student.swift
@@ -0,0 +1,8 @@
+//
+//  Student.swift
+//  StudentCourses
+//
+//  Created by Kattamuri,Vyshnavi on 4/4/23.
+//
+
+import Foundation
---
+++ StudentCourses/StudentCourses/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudentCourses
+//
+//  Created by Kattamuri,Vyshnavi on 4/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
